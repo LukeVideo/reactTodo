59120cf99d2d6e3147bb7177e41ba5c770a43790
Luke Martin <luke.martin.video@gmail.com>
2017-06-07 16:22:58 +0200

    init commit

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,34 @@
 var React = require('react')
+var TodoList = require('TodoList')
 
 var TodoApp = React.createClass({
+  getInitialState: function () {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'walk the dog'
+        },
+        {
+          id: 2,
+          text: 'Get shit done'
+        },
+        {
+          id:3,
+          text: 'go home'
+        },
+        {
+          id: 4,
+          text: 'play Guitare'
+        }
+      ]
+    }
+  },
   render: function () {
+    var {todos} = this.state
     return (
       <div>
-        TodoApp.jsx
+        <TodoList todos={todos}/>
       </div>
     )
   }
