2d617fa701d8d499f15ad3db5b960600789c7228
Luke Martin <luke.martin.video@gmail.com>
2017-06-13 11:30:52 +0200

    filter functionality and tests

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -49,8 +49,48 @@ describe('TodoAPI', () => {
       var actualTodos = TodoAPI.getTodos()
 
       expect(actualTodos).toEqual(todos)
-
     })
+  })
+  describe('filtereTodos', () =>{
+      var todos = [{
+        id: 1,
+        text: 'test some files',
+        completed: true
+      },
+      {
+        id: 2,
+        text: 'test all files',
+        completed: false
+      },
+      {
+        id: 3,
+        text: 'test all files',
+        completed: true
+      }]
+      it('sould return all items if showCompleted true', () => {
+        var filteredTodos = TodoAPI.filterTodos(todos, true, '')
+
+        expect(filteredTodos.length).toBe(3)
+      })
+      it('sould not return completed items if showCompleted false', () => {
+        var filteredTodos = TodoAPI.filterTodos(todos, false, '')
+
+        expect(filteredTodos.length).toBe(1)
+      })
+      it('should sort by completed status', () => {
+        var filteredTodos = TodoAPI.filterTodos(todos, true, '')
+        expect(filteredTodos[0].completed).toBe(false)
+      })
+      it('sould filter todos by searchText', () => {
+        var filteredTodos = TodoAPI.filterTodos(todos, true, 'all')
+
+        expect(filteredTodos.length).toBe(2)
+      })
+      it('sould return all todos if searchText is empty', () => {
+        var filteredTodos = TodoAPI.filterTodos(todos, true, '')
+
+        expect(filteredTodos.length).toBe(3)
+      })
 
   })
 })
